e738505d1a37961b9b3655f9f273796ce52b8485
Emma <eqbrown@isf.edu.hk>
2021-02-10 11:31:17 +0800

    add 404 page, header link to home page, updated favicon

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
+
+const linkStyle = {
+  color: 'black',
+  textDecoration: 'none'
+}
 
 export default function Header(props) {
   return (
       <div>
-        <h1>{props.headerText}</h1>
+        <h1><Link to="/" style={linkStyle}>{props.headerText}</Link></h1>
         <p>{props.subText}</p>
       </div>
   )
 }
+
+
