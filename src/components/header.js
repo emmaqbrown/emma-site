02c22e2b289c60c33ff579109e72513f535cb182
emmaqbrown <eqbrown@isf.edu.hk>
2021-02-10 16:55:38 +0800

    Revert "add 404 page, header link to home page, updated favicon"
    This reverts commit e738505d1a37961b9b3655f9f273796ce52b8485.

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
-
-const linkStyle = {
-  color: 'black',
-  textDecoration: 'none'
-}
 
 export default function Header(props) {
   return (
       <div>
-        <h1><Link to="/" style={linkStyle}>{props.headerText}</Link></h1>
+        <h1>{props.headerText}</h1>
         <p>{props.subText}</p>
       </div>
   )
 }
-
-
