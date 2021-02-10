8441c33ee7ff7e232cae9cde632b0336c4d679a2
emmaqbrown <eqbrown@isf.edu.hk>
2021-02-10 17:58:21 +0800

    testing paper on netlify

---
+++ gatsby-node.js
@@ -0,0 +1,8 @@
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => 
+    { if (stage === "build-html") 
+        { actions.setWebpackConfig
+            ({ module: { rules: [ { test: /node_modules\/paper/, use: loaders.null(), 
+        }, 
+    ], 
+}, }) } }
+
---
+++ src/pages/generate.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Generate from "../components/generate"
+
+export default function Paper(){
+  return(
+    <Generate></Generate>
+  )
+}
